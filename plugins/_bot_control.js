ad8b940b5204ed20f42007472cdfa50f35b0d4b1
BlackAmda <sl.amdakolla@gmail.com>
2022-12-06 00:22:49 +0530

    : )

--- plugins/_bot_control.js
+++ plugins/_bot_control.js
@@ -3,7 +3,7 @@
 * @author BlackAmda <https://github.com/BlackAmda>
 * @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
 * @link <https://github.com/BlackAmda/QueenAmdi>
-* @version 4.0.0
+* @version 4.0.1
 * @file  _bot_control.js - QueenAmdi bot control commands
 
 © 2022 Black Amda, ANTECH. All rights reserved.
@@ -181,7 +181,7 @@ AMDI({ cmd: "reset", desc: Lang.resetDESC, type: "profile", react: "🚮" }, (as
 AMDI({ cmd: "rate", desc: Lang.rateDESC, type: "primary", react: "✨" }, (async (amdiWA) => {
     let { input, prefix, reply, sender, sendButtonMsg, sendListMsg } = amdiWA.msgLayout
 
-    if (process.env.DATABASE_URL === 'local') return reply('Rating feature is not available for local databases! :(');
+    if (process.env.DATABASE_URL === 'local' || process.env.DATABASE_URL === 'vps') return reply('Rating feature is not available for local databases! :(');
 
     if (!input) {
         const botname = await getMiscData('BOTNAME');
